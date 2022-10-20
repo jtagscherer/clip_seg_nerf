30955f2d3111b817e8a99773331c72105b406336
Jan Tagscherer <jan.tagscherer@gmail.com>
2022-10-20 11:45:44 +0200

    Revert CLIP loss implementation to old state

--- losses.py
+++ losses.py
@@ -66,12 +66,6 @@ class CLIPLoss(nn.Module):
         super(CLIPLoss, self).__init__()
         self.model, self.preprocess = clip.load("ViT-B/32", device="cuda")
 
-    def forward(self, results, text):
-        '''similarities = []
-
-        for image in results['rgb']:
-            image = torch.nn.functional.upsample_bilinear(image, (224, 224))
-            similarities.push(1 - self.model(image, text)[0] / 100)
-
-        return sum(similarities)'''
+    def forward(self, image, text):
+        image = torch.nn.functional.upsample_bilinear(image, (224, 224))
         return 1 - self.model(image, text)[0] / 100
